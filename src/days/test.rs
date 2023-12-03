217ed192d8f3fa2fdb8f6234a154368eca47e092
Mitchell Marino <64488106+MitchellMarinoDev@users.noreply.github.com>
2023-12-03 01:25:32 -0600

    Fix test.rs

--- src/days/test.rs
+++ src/days/test.rs
@@ -6,10 +6,20 @@ use std::path::Path;
 /// This test can be helpful if you want to refactor your solution after you solve the puzzle.
 #[test]
 fn test_current() {
-    let result = days::DAYS[CURRENT_DAY].solve(Path::new("./input/"))
+    let result = days::DAYS[CURRENT_DAY - 1].solve(Path::new("./input/"));
 
-    assert_eq!(result.p1, SOLUTIONS[result.day].0);
-    assert_eq!(result.p2, SOLUTIONS[result.day].1);
+    assert_eq!(
+        result.p1,
+        SOLUTIONS[result.day - 1].0,
+        "solution to day {} part 1 was incorrect",
+        result.day
+    );
+    assert_eq!(
+        result.p2,
+        SOLUTIONS[result.day - 1].1,
+        "solution to day {} part 2 was incorrect",
+        result.day
+    );
 }
 
 /// Tests all implemented days other than the current day.
@@ -18,12 +28,22 @@ fn test_current() {
 fn test_other() {
     let results: Vec<_> = days::DAYS
         .iter()
-        .take(CURRENT_DAY - 1)
+        .take(CURRENT_DAY)
         .map(|d| d.solve(Path::new("./input/")))
         .collect();
 
     for result in results {
-        assert_eq!(result.p1, SOLUTIONS[result.day].0);
-        assert_eq!(result.p2, SOLUTIONS[result.day].1);
+        assert_eq!(
+            result.p1,
+            SOLUTIONS[result.day - 1].0,
+            "solution to day {} part 1 was incorrect",
+            result.day
+        );
+        assert_eq!(
+            result.p2,
+            SOLUTIONS[result.day - 1].1,
+            "solution to day {} part 2 was incorrect",
+            result.day
+        );
     }
 }
